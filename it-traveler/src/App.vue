37ab1da70b76eef30f7eb7f5d5d2e0a5fb8d4f5c
Misha <hryvinskyim@gmail.com>
2024-06-14 17:40:41 +0300

    first page

--- it-traveler/src/App.vue
+++ it-traveler/src/App.vue
@@ -1,5 +1,25 @@
+<script setup>
+  import  iButton  from './components/iButton/iButton.vue';
+</script>
 
 <template>
-  hellow Vue
+  <main class="flex h-screen">
+    <section class="flex-1 flex justify-center items-center px-5 bg-primary">
+    <div class="text-white text-center">
+      <img class="inline mb-6" src="./assets/map-pin.svg" alt="">
+      <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
+      <p class="leading-6 mb-11">
+        Простий і зручний веб лрлаток, який дозволить тобі відмічати 
+        твої улюблені місця, а також ті, в яких би ти дуже хотів побувати. 
+        Тож не зволікай і спробуй сам.
+      </p>
+      <iButton />
+    </div>
+
+  </section>
+  <section class="flex-1">
+    <img class="h-full object-cover" src="./assets/static-map.png" alt="">
+  </section>
+  </main>
 </template>
 
