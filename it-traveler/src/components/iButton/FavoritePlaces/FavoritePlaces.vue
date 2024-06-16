9821b6c42d76d8b3f157fa58571cdb762a11d2b1
Misha <hryvinskyim@gmail.com>
2024-06-16 15:03:09 +0300

    create new component FavoritePlaces

---
+++ it-traveler/src/components/iButton/FavoritePlaces/FavoritePlaces.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import FavoritePlace from '../FavoritePlace/FavoritePlace.vue';
+    import iButton from '../iButton/iButton.vue';
+</script>
+
+<template>
+    <div class="px-6 mb-4">
+        <div class="text-gray">Додані маркери</div>
+
+<FavoritePlace v-for="n in 4" :key="n"/>
+
+    </div>
+    <iButton class="w-full mt-10">Додати маркер</iButton>
+</template>
